e2772ca85dd43f04cb4f161104ec99dc2a064588
Ozias Dushimimana <odushimi@gmail.com>
2019-07-06 11:55:46 +0300

    Updates

---
+++ precache-manifest.fcc82a2128d872e76acee8b87b2acf18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c09f543df7d4464d4893b9990bda5e8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "032351fde9398aa33414",
+    "url": "/monsters-rolodex/static/css/main.a7860d5c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "032351fde9398aa33414",
+    "url": "/monsters-rolodex/static/js/main.7cbe0ec3.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
